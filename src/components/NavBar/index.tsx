8d5bb4e9651f888df8f28fd3566d7b7c60f20ffa
theAbzo <soon-ab@hotmail.com>
2025-11-18 19:34:27 +0200

    feat: adding cart icon and animations on add/remove

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,57 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 import './style.scss';
+import { useCartContext } from '../../context/useCartContext';
 
-export const Navbar = () => (
-  <nav className="navbar">
-    <div className="navbar__logo">MyShop</div>
-    <ul className="navbar__links">
-      <li><Link to="/">Products</Link></li>
-      <li><Link to="/cart">Cart</Link></li>
-    </ul>
-  </nav>
-);
+export const Navbar = () => {
+  const { cart } = useCartContext();
+
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const badgeText = totalItems > 9 ? '9+' : totalItems;
+
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">MyShop</div>
+      <ul className="navbar__links">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? 'active-link' : undefined
+            }
+          >
+            Products
+          </NavLink>
+        </li>
+        <li>
+          {totalItems > 0 ? (
+            <NavLink
+              to="/cart"
+              className={({ isActive }) =>
+                isActive ? 'active-link cart-link' : 'cart-link'
+              }
+              title={`${totalItems} item${totalItems !== 1 ? 's' : ''} in cart`}
+            >
+              <div className="cart-icon-container">
+                <ShoppingCartOutlined className="cart-icon" />
+                <span className="cart-badge" key={badgeText}>
+                  {badgeText}
+                </span>
+              </div>
+            </NavLink>
+          ) : (
+            // Disabled cart link (not clickable)
+            <span
+              className="cart-link disabled"
+              title="Your cart is empty"
+            >
+              <div className="cart-icon-container">
+                <ShoppingCartOutlined className="cart-icon" />
+              </div>
+            </span>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
